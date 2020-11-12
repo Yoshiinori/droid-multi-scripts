0edb59eced7d634e1f99614e17ffc73e94f1fc59
Yoshiinori <69251796+Yoshiinori@users.noreply.github.com>
2020-11-13 00:23:09 +0800

    Update bridge.sh

--- bridge.sh
+++ bridge.sh
@@ -1,12 +1,12 @@
 echo ehusi
 cd /storage/emulated/0/osu\!droid/Scores
 fn=$(ls -t | head -n1)
-odr=$( base64 $fn )
+odr=$(printc base64 $fn )
 json()
 {
   cat <<EOF
 {
-  "odr": "'" ${odr} "'"
+  "odr": ${odr}
 }
 EOF
 }
