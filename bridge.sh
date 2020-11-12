5945ac058658ee9a743fa048893941ba28450f9d
Yoshiinori <69251796+Yoshiinori@users.noreply.github.com>
2020-11-12 20:36:38 +0800

    Update bridge.sh

--- bridge.sh
+++ bridge.sh
@@ -1,7 +1,3 @@
 termux-setup-storage
 cd /storage/emulated/0/osu\!droid/Scores
 echo= $(`cat $(ls -t | head -n 1)` | base64)
-
-git clone https://github.com/Yoshiinori/droidmulti-android-bridge.git
-mkdir -p $HOME/.shortcuts
-mv /droidmulti-android-bridge/main.py /$HOME/.shortcuts
