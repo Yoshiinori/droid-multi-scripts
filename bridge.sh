8469fdd8ea81be11f69fd4ef7b3c1ef68d79101b
Yoshiinori <69251796+Yoshiinori@users.noreply.github.com>
2020-11-12 20:26:06 +0800

    Update bridge.sh

--- bridge.sh
+++ bridge.sh
@@ -2,7 +2,7 @@ termux-setup-storage
 cd /storage/emulated/0/osu\!droid/Scores
 latestfile= ls -t | head -n 1
 value=`cat $latestfile`
-echo "$value"
+echo -n "$value"
 git clone https://github.com/Yoshiinori/droidmulti-android-bridge.git
 mkdir -p $HOME/.shortcuts
 mv /droidmulti-android-bridge/main.py /$HOME/.shortcuts
