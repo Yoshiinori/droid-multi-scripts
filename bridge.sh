8612cd0cddd18ad4aa61671ff712d58fb8018108
Yoshiinori <69251796+Yoshiinori@users.noreply.github.com>
2020-11-12 20:47:29 +0800

    Update bridge.sh

--- bridge.sh
+++ bridge.sh
@@ -1,4 +1,4 @@
 termux-setup-storage
-cd /storage/emulated/0/osu\!droid/Scores
-fn=$( locate .odr | xargs ls -rt | tail -1)
-echo $fn
+cd /storage/emulated/0/osu\!droid/Scores | exit
+fn=$(ls -t | head -n1)
+echo "$fn"
