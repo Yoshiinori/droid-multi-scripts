94f79dd3cc899b524191fd0df906d65d247159b7
Yoshiinori <69251796+Yoshiinori@users.noreply.github.com>
2020-11-12 21:17:52 +0800

    Update bridge.sh

--- bridge.sh
+++ bridge.sh
@@ -1,7 +1,8 @@
 cd /storage/emulated/0/osu\!droid/Scores
 fn=$(ls -t | head -n1)
 odr=$( base64 $fn )
+json={"odr": $odr}
 curl -i \
 -H "Accept: application/json" \
 -H "Content-Type:application/json" \
--X POST --data {"odr": $odr} "https://javadeserialize.yoshiinori.repl.co"
+-X POST --data $json "https://javadeserialize.yoshiinori.repl.co"
