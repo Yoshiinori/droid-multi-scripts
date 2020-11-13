88781311f4cfdad5593f77c198e333a1a4129aad
Yoshiinori <69251796+Yoshiinori@users.noreply.github.com>
2020-11-13 11:18:53 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-mkdir -p $HOME/.shortcuts && cd $HOME/.shortcuts && curl https://github.com/Yoshiinori/droid-multi-scripts/releases/download/beta/send-score.sh --output try.sh
+mkdir -p $HOME/.shortcuts && cd $HOME/.shortcuts && curl https://github.com/Yoshiinori/droid-multi-scripts/releases/download/beta/send-score.sh > try.sh
 
 
 
