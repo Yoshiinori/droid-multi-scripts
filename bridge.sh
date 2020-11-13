f1029f891991219dfd7351cd04c7cc68203070b1
Yoshiinori <69251796+Yoshiinori@users.noreply.github.com>
2020-11-13 09:14:34 +0800

    Update bridge.sh

--- bridge.sh
+++ bridge.sh
@@ -1,4 +1,4 @@
 cd /storage/emulated/0/osu\!droid/Scores
 fn=$(ls -t | head -n1)
-curl -X POST -H "Content-Type: application/json" -d '{"image" : "'"$( $fn | xxd -p )"'"}' https://javadeserialize.yoshiinori.repl.co
+curl -X POST -H "Content-Type: application/json" -d '{"image" : $( $fn | xxd -p )}' https://javadeserialize.yoshiinori.repl.co
   
