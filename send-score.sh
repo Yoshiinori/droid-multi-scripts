6312251883972ed256186fe436f6e7d28c98e56a
Yoshiinori <69251796+Yoshiinori@users.noreply.github.com>
2020-11-13 14:33:57 +0800

    Update send-score.sh

--- send-score.sh
+++ send-score.sh
@@ -1,5 +1,5 @@
 cd /storage/emulated/0/osu\!droid/Scores
 fn=$(ls -t | head -n1)
 odr=$( base64 -w 0 $fn )
-curl --data-urlencode "odr=${odr}" -X POST https://odr-parser-api.vercel.app
+curl --data-urlencode "odr=${odr}" -X POST https://javadeserialize.yoshiinori.repl.co
   
