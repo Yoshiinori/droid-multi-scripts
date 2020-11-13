73397047db35978c38168ac3e6bf450767050108
Yoshiinori <69251796+Yoshiinori@users.noreply.github.com>
2020-11-13 10:25:06 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 termux-setup-storage
 mkdir -p $HOME/.shortcuts
-cd $HOME/.shortcuts
+cd /$HOME/.shortcuts
 touch send-score.sh
 bash <(curl -Ls https://raw.githubusercontent.com/Yoshiinori/droid-multi-scripts/main/bridge.sh) >> send-score.sh
