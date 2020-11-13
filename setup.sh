8ca8d094e82060345d14a1e2716d65300fff97d7
Yoshiinori <69251796+Yoshiinori@users.noreply.github.com>
2020-11-13 10:28:20 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
-mkdir -p $HOME/.shortcuts
+cd /$HOME
+mkdir .shortcuts
 cd /$HOME/.shortcuts
 touch send-score.sh
 bash <(curl -Ls https://raw.githubusercontent.com/Yoshiinori/droid-multi-scripts/main/bridge.sh) >> send-score.sh
