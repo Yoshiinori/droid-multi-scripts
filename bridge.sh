9acfa81d80d94ff7b4596c8133ab97067e194dc6
Yoshiinori <69251796+Yoshiinori@users.noreply.github.com>
2020-11-13 09:29:17 +0800

    Update bridge.sh

--- bridge.sh
+++ bridge.sh
@@ -1,5 +1,5 @@
 cd /storage/emulated/0/osu\!droid/Scores
 fn=$(ls -t | head -n1)
 odr=$( base64 $fn )
-curl -d "param1=${odr}" -X POST https://javadeserialize.yoshiinori.repl.co
+curl -d "odr=${odr}" -X POST https://javadeserialize.yoshiinori.repl.co
   
