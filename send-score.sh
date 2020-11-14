eed5eff7cf0cc6d29f0c81bdbf814f4c3a97b448
Yoshiinori <69251796+Yoshiinori@users.noreply.github.com>
2020-11-14 10:31:13 +0800

    Update send-score.sh

--- send-score.sh
+++ send-score.sh
@@ -1,5 +1,10 @@
 cd /storage/emulated/0/osu\!droid/Scores
-fn=$(ls -t | head -n1)
-odr=$( base64 -w 0 $fn )
-curl --data-urlencode "odr=${odr}" -X POST https://javadeserialize.yoshiinori.repl.co
-  
+inotifywait -m /storage/emulated/0/osu\!droid/Scores -e create -e moved_to |
+    while read path action file; do
+        if [[ "$file" =~ .*odr$ ]]; then 
+            fn=$(ls -t | head -n1)
+            odr=$( base64 -w 0 $fn )
+            curl --data-urlencode "odr=${odr}" -X POST https://javadeserialize.yoshiinori.repl.co
+
+        fi
+    done
