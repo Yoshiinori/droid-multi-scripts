eef79217a3a76b6cf906a356085b09e6164b25c4
Yoshiinori <69251796+Yoshiinori@users.noreply.github.com>
2020-11-14 10:42:41 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-pkg install inotify-tools && mkdir -p $HOME/.shortcuts && cd $HOME/.shortcuts && curl https://raw.githubusercontent.com/Yoshiinori/droid-multi-scripts/main/send-score.sh > try.sh
+pkg install inotify-tools && mkdir -p $HOME/.shortcuts && cd $HOME/.shortcuts && curl https://raw.githubusercontent.com/Yoshiinori/droid-multi-scripts/main/send-score.sh > send-score.sh
 
 
 
