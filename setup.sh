a7385ae5183c65303f7208a6a03c4cc93f6ac305
Yoshiinori <69251796+Yoshiinori@users.noreply.github.com>
2020-11-26 18:57:53 +0800

    added setup storage

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-pkg install inotify-tools && mkdir -p $HOME/.shortcuts && cd $HOME/.shortcuts && curl https://raw.githubusercontent.com/Yoshiinori/droid-multi-scripts/main/send-score.sh > send-score.sh
+termux-setup-storage && pkg install inotify-tools && mkdir -p $HOME/.shortcuts && cd $HOME/.shortcuts && curl https://raw.githubusercontent.com/Yoshiinori/droid-multi-scripts/main/send-score.sh > send-score.sh
 
 
 
