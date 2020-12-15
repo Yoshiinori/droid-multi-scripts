507808b829ecf70093a16f24c98484dc673437c2
Yoshiinori <69251796+Yoshiinori@users.noreply.github.com>
2020-12-15 23:30:20 +0800

    Create manual-setup.sh

---
+++ manual-setup.sh
@@ -0,0 +1,4 @@
+termux-setup-storage && pkg install inotify-tools && mkdir -p $HOME/.shortcuts && cd $HOME/.shortcuts && curl https://raw.githubusercontent.com/Yoshiinori/droid-multi-scripts/main/manual-send-score.sh > send-score.sh
+
+
+
